2b61a1915e9759b15b89ab4bb526c2c10b0b0e8b
Dawood <sulaimondawod@gmail.com>
2025-01-02 22:49:00 +0100

    modified the message controller setup

--- src/main/java/com/cloud/project_management_system/controller/MessageController.java
+++ src/main/java/com/cloud/project_management_system/controller/MessageController.java
@@ -1,5 +1,10 @@
 package com.cloud.project_management_system.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/messages")
 public class MessageController {
 }
